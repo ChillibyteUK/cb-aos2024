ae298ec17e1164e276691c4ad21d3910dbd1e230
LamcatUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-09-23 16:57:46 +0100

    Update cb_pdf.php

--- page-templates/blocks/cb_pdf.php
+++ page-templates/blocks/cb_pdf.php
@@ -7,13 +7,15 @@ $file_url = wp_get_attachment_url($attachment_id);
 
 $class = $block['className'] ?? null;
 
+$file_type = wp_check_filetype($file_url);
+
 ?>
 <section class="pdf_thumbnail <?=$class?>">
     <div class="container-xl text-center">
         <a class="pdf_thumbnail__card" href="<?=$file_url?>" target="_blank">
             <?php
             if ($pdf_thumbnail) {
-                echo '<img src="' . esc_url($pdf_thumbnail[0]) . '" alt="PDF Thumbnail">';
+                echo '<img src="' . esc_url($pdf_thumbnail[0]) . '" alt="Thumbnail">';
             }
             if (get_field('caption')) {
                 ?>
@@ -21,7 +23,7 @@ $class = $block['className'] ?? null;
                 <?php
             }
             ?>
-            <div class="pdf_thumbnail__size"><i class="fa-solid fa-file-pdf"></i> <span>Download PDF (<?=formatBytes($file_size)?>)</span></div>
+            <div class="pdf_thumbnail__size"><i class="fa-solid fa-file-<?=$file_type['ext']?>"></i> <span>Download <?=strtoupper($file_type['ext'])?> (<?=formatBytes($file_size)?>)</span></div>
         </a>
     </div>
 </section>
