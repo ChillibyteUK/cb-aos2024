d652587d78d068259d559c28e155c1d6fd413399
LamcatUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-12-06 16:55:50 +0000

    bread

--- inc/cb-blocks.php
+++ inc/cb-blocks.php
@@ -122,6 +122,15 @@ function acf_blocks()
             'mode'    => 'edit',
             'supports' => array('mode' => false),
         ));
+        acf_register_block_type(array(
+            'name'                => 'cb_breadcrumbs',
+            'title'                => __('CB Breadcrumbs'),
+            'category'            => 'layout',
+            'icon'                => 'cover-image',
+            'render_template'    => 'page-templates/blocks/cb_breadcrumbs.php',
+            'mode'    => 'edit',
+            'supports' => array('mode' => false),
+        ));
     }
 }
 add_action('acf/init', 'acf_blocks');
---
+++ page-templates/blocks/cb_breadcrumbs.php
@@ -0,0 +1,4 @@
+<?php
+if (function_exists('yoast_breadcrumb')) {
+    yoast_breadcrumb('<div class="container-xl"><p id="breadcrumbs">', '</p></div>');
+}
